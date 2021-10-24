80897a29f13e5c73d9b7fe996a5279f01e7d9dfb
Biraj Mainali <biraj.prathamit@gmail.com>
2021-10-24 22:01:17 +0545

    refactor : session factory loeader container

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
 const __ = document.querySelector.bind(document);
-const start = async () => {
 
-    const stream = await navigator.mediaDevices.getDisplayMedia({
-        video: {
-            mediaSource: "screen",
+const sessionFactoryLoader = async () => {
+    return {
+        stream: await navigator.mediaDevices.getDisplayMedia({video: {mediaSource: "screen",},}),
+        chunks: [],
+        renderer: __("video"),
+        toggleRecordingState: function (state = true) {
+            const startRecordElem = __(".start__recording");
+            if (state) {
+                const sessionInfoElem = __("#session--name");
+                startRecordElem.setAttribute("disabled", true);
+                startRecordElem.textContent = (!sessionInfoElem.value) ? "Recording" : sessionInfoElem.value;
+            } else {
+                startRecordElem.removeAttribute("disabled");
+                startRecordElem.textContent = " Start Recording";
+            }
         },
-    });
-    const Chunk = [];
-    const mediaRecorder = new MediaRecorder(stream);
-    mediaRecorder.ondataavailable = (e) => {
-        Chunk.push(e.data);
-    };
-    mediaRecorder.start();
-    mediaRecorder.onstop = async (e) => {
-        __("video").src = URL.createObjectURL(
-            new Blob(Chunk, {
-                type: Chunk[0].type,
-            })
-        );
-    };
+        startRecording: async function () {
+            this.toggleRecordingState();
+            const mediaRecorder = new MediaRecorder(this.stream);
+            mediaRecorder.ondataavailable = (e) => {
+                console.log(e.data);
+                this.chunks.push(e.data);
+            }
+            mediaRecorder.start();
+            mediaRecorder.onstop = async () => {
+                this.toggleRecordingState(false);
+                this.renderer.src = URL.createObjectURL(
+                    new Blob(this.chunks, {
+                        type: this.chunks[0].type,
+                    })
+                )
+            }
+        }
+
+    }
 };
+__(".start__recording").addEventListener('click', async _ => {
+    await (await sessionFactoryLoader()).startRecording()
+});
 
-
-__(".start__recording").addEventListener('click', start)
